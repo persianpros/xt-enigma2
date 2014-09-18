e729319108610c8eaea6b9443ac14d5129febcf6
satinfo <satinfo76@web.de>
2014-09-18 18:45:34 +0200

    add Atemio Device Manager

---
+++ lib/python/Plugins/SystemPlugins/AtemioDeviceManager/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import gettext
+
+def localeInit():
+	gettext.bindtextdomain("AtemioDeviceManager", resolveFilename(SCOPE_PLUGINS, "SystemPlugins/AtemioDeviceManager/locale"))
+
+def _(txt):
+	t = gettext.dgettext("AtemioDeviceManager", txt)
+	if t == txt:
+		print "[AtemioDeviceManager] fallback to default translation for:", txt
+		t = gettext.gettext(txt)
+	return t
+
+localeInit()
+language.addCallback(localeInit)
