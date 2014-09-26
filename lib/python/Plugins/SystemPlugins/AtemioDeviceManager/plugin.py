12ff9d1f36c6ec5e1092e303947a17d29d183e68
satinfo <satinfo76@web.de>
2014-09-26 02:26:21 +0200

    removed menu entry Atemio Device Manager -> in Atemio Panel

--- lib/python/Plugins/SystemPlugins/AtemioDeviceManager/plugin.py
+++ lib/python/Plugins/SystemPlugins/AtemioDeviceManager/plugin.py
@@ -5,19 +5,14 @@ from HddSetup import HddSetup
 from HddMount import HddFastRemove
 from Plugins.Plugin import PluginDescriptor
 
-def deviceManagerMain(session, **kwargs):
-	session.open(HddSetup)
-
 def deviceManagerSetup(menuid, **kwargs):
-	if menuid != "system":
-		return []
-	return [(_("Atemio Device Manager"), deviceManagerMain, "deviceManager", 5)]
+	session.open(HddSetup)
 
 def deviceManagerFastRemove(session, **kwargs):
 	session.open(HddFastRemove)
 
 
 def Plugins(**kwargs):
-	return [PluginDescriptor(name = _("Atemio Device Manager"), description = _("Format/Partition your Devices and manage Mountpoints"), where = PluginDescriptor.WHERE_MENU, fnc = deviceManagerSetup),
+	return [PluginDescriptor(name = _("Atemio Device Manager"), description = _("Format/Partition your Devices and manage Mountpoints"), where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc = deviceManagerSetup),
 			PluginDescriptor(name = _("Atemio Device Manager - Fast Mounted Remove"), description = _("Quick and safe remove for your mounted devices "), where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc = deviceManagerFastRemove)]
 
