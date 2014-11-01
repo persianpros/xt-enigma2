93e25bc7e766942cfe62b91fcf7ebb5ada473472
satinfo <satinfo76@web.de>
2014-11-01 09:31:46 +0100

    [AtemioDeviceManager] remove language and use system language

--- lib/python/Plugins/SystemPlugins/AtemioDeviceManager/__init__.py
+++ lib/python/Plugins/SystemPlugins/AtemioDeviceManager/__init__.py
@@ -3,15 +3,15 @@ from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 import gettext
 
-def localeInit():
-	gettext.bindtextdomain("AtemioDeviceManager", resolveFilename(SCOPE_PLUGINS, "SystemPlugins/AtemioDeviceManager/locale"))
+#def localeInit():
+#	gettext.bindtextdomain("AtemioDeviceManager", resolveFilename(SCOPE_PLUGINS, "SystemPlugins/AtemioDeviceManager/locale"))
 
 def _(txt):
 	t = gettext.dgettext("AtemioDeviceManager", txt)
 	if t == txt:
-		print "[AtemioDeviceManager] fallback to default translation for:", txt
+#		print "[AtemioDeviceManager] fallback to default translation for:", txt
 		t = gettext.gettext(txt)
 	return t
 
-localeInit()
-language.addCallback(localeInit)
+#localeInit()
+#language.addCallback(localeInit)
